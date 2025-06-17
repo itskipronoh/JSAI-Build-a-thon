584f5045e7e574588d4a74159fc1de576617ef52
itskipronoh <kipronoyegon68@gmail.com>
2025-06-17 18:18:07 +0300

    A local generative AI prototype
    Signed-off-by: itskipronoh <kipronoyegon68@gmail.com>

---
+++ sample.js
@@ -0,0 +1,40 @@
+import ModelClient, { isUnexpected } from "@azure-rest/ai-inference";
+import { AzureKeyCredential } from "@azure/core-auth";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+const token = process.env["GITHUB_TOKEN"];
+// const token = process.env.GITHUB_TOKEN;
+const endpoint = "https://models.github.ai/inference";
+const model = "openai/gpt-4.1";
+
+export async function main() {
+
+  const client = ModelClient(
+    endpoint,
+    new AzureKeyCredential(token),
+  );
+
+  const response = await client.path("/chat/completions").post({
+    body: {
+      messages: [
+        { role:"system", content: "You are a helpful assistant." },
+        { role:"user", content: "What is the capital of Kenya?" }
+      ],
+      temperature: 1.0,
+      top_p: 1.0,
+      model: model
+    }
+  });
+
+  if (isUnexpected(response)) {
+    throw response.body.error;
+  }
+
+  console.log(response.body.choices[0].message.content);
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
+
